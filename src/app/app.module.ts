da103d965a423f73731ca7a292b68e45438bb268
Kenny Pham <kennypham@kennys-mbp-2.attlocal.net>
2020-02-14 18:35:46 -0800

    sidebar component deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { HttpService } from "./http.service";
 import { HttpClientModule } from "@angular/common/http";
 
-import { MaterialModule } from './material/material.module';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { MyDayComponent } from './components/my-day/my-day.component';
-import { NewTaskComponent } from './components/new-task/new-task.component';
-import { ImportantComponent } from './components/important/important.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { NotfoundComponent } from './components/notfound/notfound.component';
-import { EditComponent } from './components/edit/edit.component';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MaterialModule } from "./material/material.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TasksComponent } from "./components/tasks/tasks.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MyDayComponent } from "./components/my-day/my-day.component";
+import { NewTaskComponent } from "./components/new-task/new-task.component";
+import { ImportantComponent } from "./components/important/important.component";
+import { LayoutModule } from "@angular/cdk/layout";
+import { NotfoundComponent } from "./components/notfound/notfound.component";
+import { EditComponent } from "./components/edit/edit.component";
+import { SideNavComponent } from "./components/side-nav/side-nav.component";
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule
+} from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
-    SidebarComponent,
     MyDayComponent,
     NewTaskComponent,
     ImportantComponent,
     NotfoundComponent,
     EditComponent,
-    SideNavComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
@@ -44,9 +48,9 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule,
+    MatListModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
